a75cce0bf616f96dab907b5dab3fb7c499253ae9
RafaelN9 <73660960+RafaelN9@users.noreply.github.com>
2021-07-20 17:44:35 -0300

    Teste modal Bootstrap

--- medico/atender.php
+++ medico/atender.php
@@ -15,7 +15,30 @@
         }
     </style>
   <body>
-      
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            ...
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+        </div>
+    </div>
+    </div>
+
+
+
   <div class="container-fluid " style="min-height: 100vh;">
   <div class="container-fluid" >
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -49,7 +72,7 @@
             <div class="d-flex justify-content-center w-100 align-content-center">
         
                 <div class="btn-group-vertical border w-50 p-4 rounded">
-                    <button type="button" class="btn btn-primary mb-2">Solicitar exame</button>
+                    <button type="button" class="btn btn-primary mb-2" data-toggle="modal" data-target="#exampleModal">Solicitar exame</button>
                     <button type="button" class="btn btn-primary mb-2">Resultado exame</button>
                     <button type="button" class="btn btn-primary mb-2">Diagnóstico</button>
                     <button type="button" class="btn btn-primary mb-2">Receita</button>
